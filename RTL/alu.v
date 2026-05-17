b81703607a9b97893495744dc645d2c6e5205b46
Iago <iagovitorcz@gmail.com>
2026-05-17 20:58:43 -0300

    refactoring(ALU signals) : flag zero, neg and overflow change of always block to assign block, remove temp_carry_in to use a flag inc like the carry_in signal

--- RTL/alu.v
+++ RTL/alu.v
@@ -5,12 +5,23 @@ module alu #(
     input wire [1:0] sel,
     input wire signed [WIDTH-1:0] in_a, in_b,
     output reg signed [WIDTH-1:0] alu_out,
-    output reg overflow, is_zero, is_neg
+    output is_zero,  is_neg, overflow
 );
+    // OPCODES
+    localparam AND      = 2'b00;
+    localparam NOT_B    = 2'b01;
+    localparam OR       = 2'b10;
+    localparam ADD      = 2'b11;
+
 
     reg [WIDTH:0] temporary_sum;   
     reg [WIDTH-1:0] a, b; 
-    reg carry_in_temp; 
+
+
+    assign is_zero = ~|alu_out;         // NOR (reduction)
+    assign is_neg = alu_out[WIDTH-1];   // MSB 
+    assign overflow = temporary_sum[WIDTH]; // overflow flag
+
 
     always @(*) begin : inputs_logic
 
@@ -18,35 +29,30 @@ module alu #(
         a = (!inv_a) ? a : ~a; 
 
         b = (en_b) ? in_b : {WIDTH{1'b0}};
-        carry_in_temp = (inc) ? 1'b1 : 1'b0; 
-
+        
     end
 
     always @(*) begin : alu_logic_operations
 
         casez({sel[0], sel[1]}) 
 
-            2'b00 : begin : A_and_B
+            AND : begin
                 alu_out = a & b;
             end
 
-            2'b10 : begin : A_or_B
+            OR : begin
                 alu_out = a | b;
             end  
 
-            2'b01 : begin : NOT_B
+            NOT_B : begin
                 alu_out = ~b;
             end 
 
-            2'b11 : begin : A_plus_B // change later to match exactly the especifications behavior
-                temporary_sum = a + b + carry_in_temp;
+            ADD : begin 
+                temporary_sum = a + b + inc;
                 alu_out = temporary_sum[WIDTH-1:0];
-                overflow = temporary_sum[WIDTH];
             end 
         endcase 
-
-        is_zero = (alu_out == 0) ? 1'b1 : 1'b0;
-        is_neg = (alu_out[WIDTH-1]) ? 1'b1 : 1'b0;
 
     end
 
